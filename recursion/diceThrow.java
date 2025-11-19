95e4084a32e47e7dbcaccc59eaf73defa8fe785e
Fouziya-1903 <118104880+Fouziya-1903@users.noreply.github.com>
2025-11-19 23:00:37 +0530

    diceThrow return type arraylist

--- recursion/diceThrow.java
+++ recursion/diceThrow.java
@@ -1,6 +1,10 @@
+import java.util.ArrayList;
+
 public class diceThrow {
     public static void main(String[] args) {
         dice("", 4);
+        ArrayList<String> result = diceRet("", 4);
+        System.out.println(result);
     }
     public static void dice(String p, int target){
         if(target <= 0){
@@ -11,5 +15,22 @@ public class diceThrow {
         for(int i = 1; i<=6 && i<=target; i++){
             dice(p+i, target-i);
         }
+    }
+
+    public static ArrayList<String> diceRet(String p, int target){
+        if(target <= 0){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+
+        ArrayList<String> list = new ArrayList<>();
+
+        for(int i = 1; i<=6 && i<=target; i++){
+            
+            list.addAll(diceRet(p+i, target-i));
+        }
+
+        return list;
     }
 }
