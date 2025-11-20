02161174aff01ccb7d3922ec65a8963f028b75e1
Fouziya-1903 <118104880+Fouziya-1903@users.noreply.github.com>
2025-11-20 17:45:05 +0530

    diagonal paths

--- recursion/paths.java
+++ recursion/paths.java
@@ -8,6 +8,8 @@ public class paths {
         pathString("",3,3);
         ArrayList<String> result = pathRet("", 3, 3);
         System.out.println(result);
+        ArrayList<String> diagPath = pathRetDiag("", 3, 3);
+        System.out.println(diagPath);
     }
     public static int countPath(int r, int c){
         if(r == 1 || c == 1){
@@ -20,16 +22,20 @@ public class paths {
         return left + right;
     }
     public static void pathString(String p, int r, int c){
-        if(r == 1 || c == 1){
+        if(r == 1 && c == 1){
             System.out.println(p);
             return;
         }
-
-        pathString(p +"D",r-1, c);
-        pathString(p + "R", r, c-1);
+        if(r>1){
+            pathString(p +"D",r-1, c);
+        }
+        if(c>1){
+            pathString(p + "R", r, c-1);
+        }
+        
     }
     public static ArrayList<String> pathRet(String p, int r, int c){
-        if(r == 1 || c == 1){
+        if(r == 1 && c == 1){
             ArrayList<String> list = new ArrayList<>();
             list.add(p);
             return list;
@@ -37,8 +43,33 @@ public class paths {
 
         ArrayList<String> list = new ArrayList<>();
 
-        list.addAll(pathRet(p +"D",r-1, c));
-        list.addAll(pathRet(p + "R", r, c-1));
+        if(r>1){
+            list.addAll(pathRet(p +"D",r-1, c));
+        }
+        if(c>1){
+            list.addAll(pathRet(p + "R", r, c-1));
+        }
+
+        return list;
+    }
+    public static ArrayList<String> pathRetDiag(String p, int r, int c){
+        if(r == 1 && c == 1){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+
+        ArrayList<String> list = new ArrayList<>();
+
+        if(r>1){
+            list.addAll(pathRetDiag(p +"V",r-1, c));
+        }
+        if(r>1 && c > 1){
+            list.addAll(pathRetDiag(p + "d",r-1,c-1));
+        }
+        if(c>1){
+            list.addAll(pathRetDiag(p + "H", r, c-1));
+        }
 
         return list;
     }
