dfc3364bc16bf551c1f48d11f284a002ee7c14eb
Fouziya-1903 <118104880+Fouziya-1903@users.noreply.github.com>
2025-11-25 01:18:52 +0530

    inheritance 2

--- OOP/fouzi/Main.java
+++ OOP/fouzi/Main.java
@@ -3,6 +3,8 @@ package fouzi;
 public class Main {
     public static void main(String[] args) {
         Box box1 = new Box();
-        System.out.println(box1.h+" "+ box1.w);  
+        BoxWeight box2 = new BoxWeight();
+        System.out.println(box1.h+" "+ box1.w); 
+        System.out.println(box2.h+" "+ box2.w); 
     }
 }
