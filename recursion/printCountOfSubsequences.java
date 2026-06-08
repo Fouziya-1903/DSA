61452e86c537a1c74aeeb7f145e9afdcb272f491
Fouziya-1903 <118104880+Fouziya-1903@users.noreply.github.com>
2026-06-08 17:23:30 +0530

    fixing count of Subsequences

--- recursion/printCountOfSubsequences.java
+++ recursion/printCountOfSubsequences.java
@@ -4,10 +4,10 @@ class printCountOfSubsequences {
         int[] nums = {1,2,1};
         List<Integer> path = new ArrayList<>();
         int targetSum = 2;
-        int countSubsequence = printCountOfSubsequences(0, new ArrayList<>(), nums, 0, targetSum);
+        int countSubsequence = printCountOfSubsequences(0, nums, 0, targetSum);
         System.out.print(countSubsequence);
     }
-    public static int printCountOfSubsequences(int index, List<Integer> path, int[] nums, int sum, int targetSum){
+    public static int printCountOfSubsequences(int index, int[] nums, int sum, int targetSum){
         if(index == nums.length){
             if(sum == targetSum){
                 return 1;
@@ -15,13 +15,12 @@ class printCountOfSubsequences {
                 return 0;
             }
         }
-        path.add(nums[index]);
-        sum += nums[index];
-        int l = printCountOfSubsequences(index+1, path, nums, sum, targetSum);
         
-        path.remove(path.size()-1);
+        sum += nums[index];
+        int l = printCountOfSubsequences(index+1, nums, sum, targetSum);
+    
         sum -= nums[index];
-        int r = printCountOfSubsequences(index+1, path, nums, sum, targetSum);
+        int r = printCountOfSubsequences(index+1, nums, sum, targetSum);
         
         return l + r;
     }
